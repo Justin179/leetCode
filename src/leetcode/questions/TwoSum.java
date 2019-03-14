5aeb31f2291aa21e0bd68bac2512c732bce88ae6
Justin Chen <justin.uob@gmail.com>
2019-03-14 23:39:01 +0800

    first commit

---
+++ src/leetcode/questions/TwoSum.java
@@ -0,0 +1,22 @@
+package leetcode.questions;
+
+import java.util.Arrays;
+
+public class TwoSum {
+
+	public static int[] twoSum(int[] nums, int target){
+		
+		
+		return new int[]{-1,-1};
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		int[] result = twoSum(new int[]{2,7,11,15},9);
+		
+		System.out.println(Arrays.toString(result));  
+
+	}
+
+}
